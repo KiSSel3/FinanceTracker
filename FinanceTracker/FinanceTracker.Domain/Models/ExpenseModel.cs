02de8e050d9ddaf5913e232d19ebbb8203aae41c
KiSSel3 <andreyk042004@gmail.com>
2023-11-01 17:12:27 +0300

    Base models and auxiliary classes were created

---
+++ FinanceTracker/FinanceTracker.Domain/Models/ExpenseModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceTracker.Domain.Models
+{
+    public class ExpensesModel : BaseModel
+    {
+        public ExpensesModel() =>
+            (ExpensesTypeId, CreationData, Amount) = (Guid.Empty, DateOnly.MinValue, 0);
+        public ExpensesModel(Guid expensesTypeId, decimal amount, DateOnly creationData) =>
+            (ExpensesTypeId, CreationData, Amount) = (expensesTypeId, creationData, amount);
+        public Guid ExpensesTypeId { get; set; }
+        public DateOnly CreationData { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
